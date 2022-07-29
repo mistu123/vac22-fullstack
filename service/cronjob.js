96ec54a86b31abb5e0d897ed0935b97dd72fb0b7
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2022-07-30 00:55:43 +0530

    Updated cron job file

--- service/cronjob.js
+++ service/cronjob.js
@@ -2,9 +2,10 @@ import User from '../model/user';
 import db from "../database";
 import util from "../helper/util";
 const fs = require("fs");
-
+// cron module
 const cron = require('node-cron');
-const task = cron.schedule('*/30 * * * * *', () => {
+
+const task = cron.schedule('* * * * *', () => {
     sendEmailToUser();
 });
 task.start();
@@ -15,13 +16,14 @@ const sendEmailToUser = () => {
     const user = new User();
     db.query(user.fetchUserList(), async (error, result) => {
         if (!error) {
+            // if users available
             if (result.length) {
                 result.forEach(async (key) => {
-                    console.log(key);
                     await util.emailSend(key)
                 });
             }
         } else {
+            // if error in query
             fs.appendFile("logs.txt", error, function(err) {
                 if (err) throw err;
                 console.log("Status Logged!");
