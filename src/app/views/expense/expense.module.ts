34634ae414e816403408ffe1546bc7f9c0eca359
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2022-08-01 18:44:15 +0530

    Updated Dashboard view

--- src/app/views/expense/expense.module.ts
+++ src/app/views/expense/expense.module.ts
@@ -5,9 +5,10 @@ import { ExpenseRoutingModule } from './expense-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ManageComponent } from './manage/manage.component';
 import { LoadingBarModule } from '@ngx-loading-bar/core';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [DashboardComponent, ManageComponent],
-  imports: [CommonModule, ExpenseRoutingModule, LoadingBarModule, FormsModule],
+  imports: [CommonModule, ExpenseRoutingModule, LoadingBarModule, FormsModule, TooltipModule.forRoot()],
 })
 export class ExpenseModule {}
